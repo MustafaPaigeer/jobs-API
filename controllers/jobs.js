78aebfff809060eed2e24479a453a232c03e9379
MustafaPaigeer <mustafapaigeer@live.com>
2022-07-19 22:26:07 -0700

    add update single job functionality

--- controllers/jobs.js
+++ controllers/jobs.js
@@ -8,26 +8,45 @@ const getAllJobs = async (req, res) => {
 }
 
 const getJob = async (req, res) => {
-  const { user: { userId }, 
-  params: { id: jobId } 
-} = req
+  const { user: { userId },
+    params: { id: jobId }
+  } = req
 
   const job = await Job.findOne({
     _id: jobId, createdBy: userId
   })
+
   if (!job) {
     throw new NotFoundError(`No job with id ${jobId}`)
   }
   res.status(StatusCodes.OK).json({ job })
 }
+
 const createJob = async (req, res) => {
   req.body.createdBy = req.user.userId
   const job = await Job.create(req.body)
   res.status(StatusCodes.CREATED).json({ job })
 }
+
 const updateJob = async (req, res) => {
-  res.send('update jobs')
+  const {
+    body: { company, position },
+    user: { userId },
+    params: { id: jobId },
+  } = req
+
+  if (company === '' || position === '') {
+    throw new BadRequestError('Company or Position fields cannot be empty')
+  }
+  const job = await Job.findByIdAndUpdate({ _id: jobId, createdBy: userId }, req.body, { new: true, runValidator: true }
+  )
+
+  if (!job) {
+    throw new NotFoundError(`No job with id ${jobId}`)
+  }
+  res.status(StatusCodes.OK).json({ job })
 }
+
 const deleteJob = async (req, res) => {
   res.send('delete jobs')
 }
