ed176c790655dfed0ee48a8bc3e055ac6d91ead0
MustafaPaigeer <mustafapaigeer@live.com>
2022-07-20 21:56:10 -0700

    add security packages

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
 require('dotenv').config();
 require('express-async-errors');
+
+// Security Packages
+
+const helmet = require('helmet');
+const cors = require('cors');
+const xss = require('xss-clean');
+const rateLimiter = require('express-rate-limit');
+
 const express = require('express');
 const app = express();
 const authenticateUser = require('./middleware/authentication')
@@ -11,17 +19,28 @@ const connectDB = require('./db/connect')
 const authRouter = require('./routes/auth')
 const jobsRouter = require('./routes/jobs')
 
-
 // error handler
 const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
 
+app.set('trust proxy', 1);
+app.use(
+  rateLimiter({
+    windowMS: 15 * 60 * 1000, // 15 minutes
+    max: 100, // limit each IP to 100 requests per windowMs
+  })
+);
+
 app.use(express.json());
+app.use(helmet());
+app.use(cors());
+app.use(xss());
+
 // extra packages
 
 // routes
 app.use('/api/v1/auth', authRouter)
-app.use('/api/v1/jobs',authenticateUser, jobsRouter)
+app.use('/api/v1/jobs', authenticateUser, jobsRouter)
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
